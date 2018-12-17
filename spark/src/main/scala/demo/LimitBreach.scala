e4f5fc763f1c67a2d3b588e14ee6706feae02157
Pakkirisamy <vpakkirisamy@paypal.com>
2018-12-17 10:15:06 +0530

    21st Commit

--- spark/src/main/scala/demo/LimitBreach.scala
+++ spark/src/main/scala/demo/LimitBreach.scala
@@ -32,7 +32,7 @@ object LimitBreach {
       .set(ConfigurationOptions.ES_INDEX_AUTO_CREATE, "true")
       .set(ConfigurationOptions.ES_NET_HTTP_AUTH_USER, "elastic")
       .set(ConfigurationOptions.ES_NET_HTTP_AUTH_PASS, "Fiw8g63BzNEOeaWYj8ESNY5d")
-      .set(ConfigurationOptions.ES_NODES_WAN_ONLY, "true")
+      .set(ConfigurationOptions.ES_NODES_WAN_ONLY, "false")
       .set(ConfigurationOptions.ES_NODES_INGEST_ONLY, "true")
       .set(ConfigurationOptions.ES_NODES_DATA_ONLY, "true")
       .set(ConfigurationOptions.ES_NODES_DISCOVERY, "true")
