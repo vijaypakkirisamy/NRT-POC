b8fa7d74839d485b6d9fe307327f4a321552d56e
Pakkirisamy <vpakkirisamy@paypal.com>
2018-12-18 21:48:40 +0530

    random 2

--- spark/src/main/scala/demo/LimitBreach.scala
+++ spark/src/main/scala/demo/LimitBreach.scala
@@ -15,7 +15,7 @@ import org.apache.spark.streaming.pubsub.{PubsubUtils, SparkGCPCredentials}
 import org.apache.spark.{SparkConf, SparkContext}
 import org.elasticsearch.hadoop.cfg.ConfigurationOptions
 import org.elasticsearch.spark.streaming.EsSparkStreaming
-
+import org.elasticsearch.spark._
 import scala.collection.Seq
 
 
@@ -27,24 +27,29 @@ object LimitBreach {
   : StreamingContext = {
 
     // [START stream_setup]
-    val sparkConf = new SparkConf().setAppName("LimitBreachtags").set(ConfigurationOptions.ES_NODES, "c4b88b17a3194a64b9e04a70b8613e5b.us-central1.gcp.cloud.es.io")
-      .set(ConfigurationOptions.ES_PORT, "443")
-      .set(ConfigurationOptions.ES_INDEX_AUTO_CREATE, "true")
-      .set(ConfigurationOptions.ES_NET_HTTP_AUTH_USER, "elastic")
-      .set(ConfigurationOptions.ES_NET_HTTP_AUTH_PASS, "Fiw8g63BzNEOeaWYj8ESNY5d")
+//    val sparkConf = new SparkConf()
+//      .setAppName("LimitBreachtags")
+//      .set(ConfigurationOptions.ES_NODES, "c4b88b17a3194a64b9e04a70b8613e5b.us-central1.gcp.cloud.es.io")
+//      .set(ConfigurationOptions.ES_PORT, "443")
+//      .set(ConfigurationOptions.ES_INDEX_AUTO_CREATE, "true")
+//      .set(ConfigurationOptions.ES_NET_HTTP_AUTH_USER, "elastic")
+//      .set(ConfigurationOptions.ES_NET_HTTP_AUTH_PASS, "Fiw8g63BzNEOeaWYj8ESNY5d")
 
-      //.set(ConfigurationOptions.ES_NODES_WAN_ONLY, "true")
-     // .set(ConfigurationOptions.ES_NODES_INGEST_ONLY, "true")
-   //   .set(ConfigurationOptions.ES_NODES_DATA_ONLY, "true")
-     // .set(ConfigurationOptions.ES_NODES_DISCOVERY, "true")
-     // .set(ConfigurationOptions.ES_NODES_CLIENT_ONLY, "false")
+    val conf = new SparkConf().setAppName("LimitsBreach")
+                              //.setMaster("local[*]")
+                              .set("es.index.auto.create", "true")
+                              .set("es.nodes","c4b88b17a3194a64b9e04a70b8613e5b.us-central1.gcp.cloud.es.io")
+                              .set("es.port", "443")
+                              .set("es.http.timeout", "5m")
+                              .set("es.scroll.size", "50")
 
+    val sc = new SparkContext(conf)
 
-    val sc = new SparkContext(sparkConf)
+   // val sc = new SparkContext(sparkConf)
     val ssc = new StreamingContext(sc, Seconds(slidingInterval.toInt))
 
     // Set the checkpoint directory
-    val yarnTags = sparkConf.get("spark.yarn.tags")
+    val yarnTags = conf.get("spark.yarn.tags")
     val jobId = yarnTags.split(",").filter(_.startsWith("dataproc_job")).head
     ssc.checkpoint(checkpointDirectory + '/' + jobId)
 
@@ -84,10 +89,11 @@ object LimitBreach {
 
     println("Stage 3 is :" + mstream.print())
 
+
     val rdd = sc.makeRDD(Seq(mstream))
-    val microbatches = scala.collection.mutable.Queue(rdd)
-    val dstream = ssc.queueStream(microbatches)
-    EsSparkStreaming.saveToEs(dstream, "breaches/trandtls")
+   val microbatches = scala.collection.mutable.Queue(rdd)
+   val dstream = ssc.queueStream(microbatches)
+    EsSparkStreaming.saveToEs(dstream, "limitbreaches/transacdtls")
     println("Stage 4 is Data Pumped to ElasticSearch Cluster.. Please check..")
 
     ssc
